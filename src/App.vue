a6bd7d43fd61857d7265fe53222f6e38c0b61785
raissalotin <rahlotin@gmail.com>
2023-03-20 11:03:49 -0300

    g

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const professor = ref({
+    nome: 'Eduardo da Silva',
+    disciplinas: [
+        'Desenvolvimento Web II', 
+        'Desenvolvimento de Aplicativos para Dispositivos Móveis',
+    ]
+})
+
+const possuiDisciplinas = computed(() => {
+    return professor.value.disciplinas.length > 0 ? 'Sim' : 'Não'
+})
+</script>
+
+<template>
+    <div>
+        <h1>Professor</h1>
+        <p>Nome: {{ professor.nome }}</p>
+        <p>Possui disciplinas: {{ possuiDisciplinas }}</p>
+    </div>
+</template>
